249b13c46878a80a43b088bddb79409ad35d1736
KonradSchieban <konrad.schieban@gmail.com>
2016-09-04 12:56:20 +0200

    "Create" functionality added for new items

--- application.py
+++ application.py
@@ -39,7 +39,7 @@ def catalog_root_page():
 
     categories = session.query(Category).all()
 
-    latest_items = session.query(Item).order_by(desc(Item.created_date)).all()
+    latest_items = session.query(Item).order_by(desc(Item.created_date)).limit(10).all()
 
     return render_template('catalog_root_page.html',
                            categories=categories,
@@ -67,6 +67,54 @@ def item_page(category_name, item_name):
     item = session.query(Item).filter_by(name=item_name, category_id=category_obj.id).one()
 
     return render_template('item.html', item=item)
+
+
+@app.route('/catalog/new', methods=['GET', 'POST'])
+def add_item_page():
+
+    if request.method == 'POST':
+
+        # Get all request parameter
+        item_name = request.form['item_name']
+        item_description = request.form['item_description']
+        item_price = request.form['item_price']
+        item_category = request.form['item_category']
+
+        # TODO: Query for user
+        user_obj = session.query(User).filter_by(email="konrad.schieban@gmail.com").one()
+        category_obj = session.query(Category).filter_by(name=item_category).one()
+
+        new_item = Item(name=item_name,
+                        user=user_obj,
+                        category=category_obj,
+                        description=item_description,
+                        price=item_price)
+        session.add(new_item)
+        session.commit()
+
+        return redirect(url_for('category_page', category=item_category))
+
+    else:
+        categories = session.query(Category).all()
+        return render_template('add_item.html', categories=categories)
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/edit')
+def edit_item_page(category_name, item_name):
+
+    category_obj = session.query(Category).filter_by(name=category_name).one()
+    item = session.query(Item).filter_by(name=item_name, category_id=category_obj.id).one()
+
+    return render_template('edit_item.html', item=item)
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/delete')
+def delete_item_page(category_name, item_name):
+
+    category_obj = session.query(Category).filter_by(name=category_name).one()
+    item = session.query(Item).filter_by(name=item_name, category_id=category_obj.id).one()
+
+    return render_template('delete_item.html', item=item)
 
 
 if __name__ == '__main__':
